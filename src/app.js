ea931fb5020a0c653342879830bd50206c4ee66f
kimopreis <85667171+kimopreis@users.noreply.github.com>
2022-03-07 19:07:17 +0100

    updated week8 Lesson 4

--- src/app.js
+++ src/app.js
@@ -54,7 +54,35 @@ function formatTime(time) {
 let timeElement = document.querySelector("#current-time");
 timeElement.innerHTML = formatTime(now);
 
-// feature 2
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function showWeather(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector("#currentTemp").innerHTML = Math.round(
